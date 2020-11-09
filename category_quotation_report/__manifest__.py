49a2adcd8169d177d06f3e632f22f01ff28815de
arunparavoor <41280036+arunparavoor@users.noreply.github.com>
2020-11-09 10:37:01 +0400

    Add files via upload

---
+++ category_quotation_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Category Wise Vendor Quotation Report',
+    'version': '1.0',
+    'category': 'Purchase',    
+    'author': 'Arun Reghu Kumar',
+    'website':'http://tech4logic.wordpress.com/',
+    'depends': ['base','purchase','purchase_requisition','sale_stock'],
+    'summary': 'Product Category wise Puchase Quotation Report',
+    'description': "Product Category Wise Vendor Quotation Report",
+    'data': [
+        "security/ir.model.access.csv",
+        "wizard/category_quotation_report.xml",
+    ],
+	'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
